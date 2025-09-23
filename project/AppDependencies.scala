7f8f0146b0e705156f5a67504730ba6c45abf673
Jemma Edwards <87577882+JemmaEdwards@users.noreply.github.com>
2025-09-23 13:56:04 +0100

    [RORO-6226][JE] upgrades hmrcMongoVersion to 2.7.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.1.0"
-  private val hmrcMongoVersion = "2.6.0"
+  private val hmrcMongoVersion = "2.7.0"
   private val playVersion      = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
